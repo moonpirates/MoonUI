9a85ce654b41ddd3065d0c783bc75b3fd5da7465
Jordi <>
2021-04-25 12:33:45 +0200

    Initial Commit

---
+++ MoonUI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MoonUI
+//
+//  Created by Jordi Scharp on 25/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
